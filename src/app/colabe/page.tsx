2ef1c809cfdeb40be587b51070573a3da20b6c24
MrSpock182 <klebernunes182@gmail.com>
2025-09-08 20:50:23 -0300

    fluxo de pagamento

--- src/app/colabe/page.tsx
+++ src/app/colabe/page.tsx
@@ -1,12 +1,12 @@
 export default function Colabe() {
   return (
     <div className="min-h-screen bg-black flex flex-col items-center justify-start pt-10 px-4">
-      <h1 className="text-4xl font-bold text-white mb-6">Obrigado</h1>
+      <h1 className="text-4xl font-bold text-white mb-6">Lindo</h1>
 
       <div className="w-full max-w-md h-[70vh] flex justify-center items-center overflow-hidden rounded shadow-lg">
         <img
-          src="/img/obrigado.jpeg"
-          alt="Obrigado"
+          src="/img/lindo.jpeg"
+          alt="Lindo"
           className="object-contain h-full w-full"
         />
       </div>
